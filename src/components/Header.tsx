ebcb022da6c15634a9b81dfa8ef882feaa413125
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2024-07-24 00:19:26 +0200

    Contact Form finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
     return (
         <header className="py-8 xl:py-12 text-white ">
             <div className="container mx-auto flex justify-between items-center">
-                <Link href={""}>
+                <Link href={"/"}>
                     <h1 className="text-4xl font-semibold ">
                         Kaiser<span className="text-accent">.</span>
                     </h1>
