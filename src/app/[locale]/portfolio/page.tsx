ad7111544a37c7d6c75e226208ab1093e8eb4041
Rahil Kaiser <95457266+rahilkaiser@users.noreply.github.com>
2024-09-18 01:46:55 +0200

    finished skills

--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -148,7 +148,7 @@ export default function Portfolio() {
                     rel="noopener noreferrer" 
                     className="relative group text-accent transition-colors duration-300 flex items-center"
                   >
-                    Projekt ansehen
+                    {trans('viewProject')}
                     <BsArrowUpRight size={16} className="ml-2" />
                     <span className="absolute bottom-0 left-0 h-0.5 bg-accent w-0 group-hover:w-full transition-all duration-500 ease-in-out" />
                   </Link>
@@ -158,7 +158,7 @@ export default function Portfolio() {
                     rel="noopener noreferrer" 
                     className="relative group text-accent transition-colors duration-300 flex items-center"
                   >
-                    GitHub
+                    {trans('githubRepository')}
                     <BsGithub size={16} className="ml-2" />
                     <span className="absolute bottom-0 left-0 h-0.5 bg-accent w-0 group-hover:w-full transition-all duration-500 ease-in-out" />
                   </Link>
